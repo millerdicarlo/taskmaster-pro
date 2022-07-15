ed2f74fdfbba70755c4eafeb9d0e85e0572793cf
millerd <millerdicarlo@gmail.com>
2022-07-14 20:50:17 -0400

    enhancements/touch ups added

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,23 +63,26 @@ var auditTask = function(taskEl) {
   }
 };
 
+//drag and drop/sortable feature
 $(".card .list-group").sortable({
+  // enable dragging across lists
   connectWith: $(".card .list-group"),
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
-
   activate: function(event) {
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event) {
 // array to store the task data in
@@ -91,18 +94,17 @@ $(this).children().each(function() {
     .find("p")
     .text()
     .trim();
-
   var date = $(this)
     .find("span")
     .text()
     .trim();    
-
   // add task data to the temp array as an object
   tempArr.push({
     text: text,
     date: date
   });
 });
+
   // trim down list's ID to match object property
 var arrName = $(this)
 .attr("id")
@@ -116,6 +118,7 @@ saveTasks();
 console.log(tempArr);
   }
 });
+
 
 $("#trash").droppable({
   accept: ".card .list-group-item",
@@ -149,7 +152,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -285,3 +288,8 @@ $("#remove-tasks").on("click", function() {
 loadTasks();
 
 
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
